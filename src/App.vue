80c43e3ee0c544723499b8f77700a1c19233c2c2
Lucas Campos Ferreira <66262807+LucasCamposFerreira@users.noreply.github.com>
2024-12-06 14:10:17 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -122,10 +122,10 @@ export default {
         ];
       } else if (preset === "lucas") {
         this.categoriasLucas = [
-          { nome: "Custo", percentual: 41.00 },
+          { nome: "Custo", percentual: 42.00 },
           { nome: "Investimento", percentual: 52.00 },
           { nome: "Se precisar", percentual: 3.00 },
-          { nome: "Lazer", percentual: 4.00 },
+          { nome: "Lazer", percentual: 3.00 },
         ];
       }
     },
